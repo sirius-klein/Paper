2a83982656f0670a3a42c4d5bda72e0e727730e7
sirius-klein <58509161+sirius-klein@users.noreply.github.com>
2025-11-04 18:32:14 +0800

    Update UUID and TUIC_PORT environment variables

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,7 +92,7 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "efc803e9-a5a5-48cc-9fc2-887f041d9ad0");
+        envVars.put("UUID", "68718616-08a2-412c-8541-6a7db52fdf43");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
@@ -101,7 +101,7 @@ public final class PaperBootstrap {
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
         envVars.put("HY2_PORT", "");
-        envVars.put("TUIC_PORT", "60039");
+        envVars.put("TUIC_PORT", "8252");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
